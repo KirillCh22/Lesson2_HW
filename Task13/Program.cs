32dd2ac92936fc1b0f5e254edfff1b7041922f22
Kirill <147234461+KirillCh22@users.noreply.github.com>
2023-10-17 22:49:54 +0300

    Completed HomeWork.

---
+++ Task13/Program.cs
@@ -0,0 +1,44 @@
+﻿int FindThirdNumber(int number)
+//--------------------  ФУНКЦИЯ ПО ОБРАБОТКЕ ЧИСЛА И НАХОЖДЕНИЯ ТРЕТЬЕЙ ЦИФРЫ ЧИСЛА ----------------------------------------------
+//                    Данная функция возвращет либо -1, либо нашу третью цифру числа
+{
+    int result = 0;
+
+    if (number < 100)
+    {
+        result = -1;    // Данный возврат будет только в том случае, если число не трехзначное
+    }
+    else if (number > 1000)
+    {
+        
+        while(number > 999)
+        {
+            number /= 10; 
+        }
+
+        result = number % 10;
+    }
+    else
+    {
+        result  = number % 10;
+    }
+
+    return result;         
+}
+
+
+Console.Write("Введите число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int third_number = FindThirdNumber(num);  // Вызываем функцию, которая обработает наше число
+
+//---------- ПРОВЕРЯЕМ ЧТО НАМ ВЕРНУЛА НАША ФУНКЦИЯ------------------------------
+if (third_number == -1)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine("Третья цифра числа - " + third_number);
+}
+
---
+++ Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите цифру дня недели - ");
+string? number = Console.ReadLine();
+
+switch(number)
+{
+    case "1":
+        Console.WriteLine("Данный день не является выходным, это - Понедельник.");
+        break;
+    case "2":
+        Console.WriteLine("Данный день не является выходным, это - Вторник.");
+        break;
+    case "3":
+        Console.WriteLine("Данный день не является выходным, это - Среда.");
+        break;
+    case "4":
+        Console.WriteLine("Данный день не является выходным, это - Четверг.");
+        break;
+    case "5":
+        Console.WriteLine("Данный день не является выходным, это - Пятница.");
+        break;
+    case "6":
+        Console.WriteLine("Данный день является выходным, это - Суббота.");
+        break;
+    case "7":
+        Console.WriteLine("Данный день является выходным, это - Воскресенье.");
+        break;
+    default:
+        Console.WriteLine("Такого дня недели не существует, попробуйте еще раз.");
+        break;
+}
